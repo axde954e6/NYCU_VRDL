3127ecde28f6dba91162caf28620dc458189811b
axde954e6 <58110019+axde954e6@users.noreply.github.com>
2021-11-19 21:54:34 +0800

    Update get_label.m

--- HW2/get_label.m
+++ HW2/get_label.m
@@ -13,14 +13,14 @@ for i = 1: length(digitStruct)
         [total_height, total_width, channel] = size(im);
         x_center = digitStruct(i).bbox(j).left + (digitStruct(i).bbox(j).width / 2);
         x_center = x_center / total_width;
-        y_center= digitStruct(i).bbox(j).top + (digitStruct(i).bbox(j).height / 2);
+        y_center = digitStruct(i).bbox(j).top + (digitStruct(i).bbox(j).height / 2);
         y_center = y_center / total_height;
         height = digitStruct(i).bbox(j).height;
         height = height / total_height;
         width = digitStruct(i).bbox(j).width;
         width = width / total_width;
         label = digitStruct(i).bbox(j).label;
-        if (label==10)
+        if (label == 10)
             label = 0;
         end
         fprintf(fid, '%d %f %f %f %f\n', label, x_center, y_center, width, height);
